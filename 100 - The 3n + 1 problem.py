7723beb84a0fc122fb3208abe8c1cf0dc71b96fe
danieru1 <dani_danielsilva@hotmail.com>
2018-12-12 00:57:20 -0200

    done

--- 100 - The 3n + 1 problem.py
+++ 100 - The 3n + 1 problem.py
@@ -1,4 +1,18 @@
-i = int(input(""))
-j = int(input(""))
-print(i,j)
-print("aluelia")
+while(1):
+    entrada = input()
+    if not entrada:
+        break
+    i, j = map(int, entrada.split())
+    maior = 0
+    for n in range(i,j+1):
+        cl=0
+        while 1:
+            cl+=1
+            if n==1: break
+            if n%2==0:
+                n = n // 2
+            else:
+                n = 3 * n + 1
+        if(cl>maior):
+            maior = cl
+    print(f"{i} {j} {maior}")
